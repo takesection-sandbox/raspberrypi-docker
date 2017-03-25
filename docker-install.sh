d897e8bd9d5904586a60d5ac3d119185ec5f47f5
S Shoji <takesection@gmail.com>
2017-03-25 11:41:07 +0900

    Fix docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo sh -c "curl -sSL https://get.docker.com | sh"
 sudo usermod -aG docker $USER
 
+sudo apt-get install python-pip -y
+sudo pip install docker-compose
+
 curl -s "https://get.sdkman.io" | bash
-source `pwd`/.sdkman/bin/sdkman-init.sh
+source ~/.sdkman/bin/sdkman-init.sh
 
 sdk install sbt
 sdk install scala
